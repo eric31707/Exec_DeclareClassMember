19242578cd96f7e0c5e3f83305e4d089114502f2
eric31707 <eric31707@DESKTOP-95TUUP6>
2022-10-24 23:03:54 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			FileManager saver=new FileManager();
+			saver.Save("txt","words");
+		}
+	}
+	class FileManager
+	{
+		public void Save(string txt, string words)
+		{
+		
+		}
+	}
+
+}
